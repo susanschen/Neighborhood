3dfbf10639ef85d3cb3b76aef43c49455bfe2257
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-09-22 13:51:23 -0400

    Apply new map style

--- app.js
+++ app.js
@@ -4,73 +4,113 @@ var map;
 var markers = [];
 
 function initMap() {
-  // Create a styles array to use with the map.
-  var styles = [
-    {
-      featureType: 'water',
-      stylers: [
-        { color: '#19a0d8' }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { color: '#ffffff' },
-        { weight: 6 }
-      ]
-    },{
-      featureType: 'administrative',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { color: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.stroke',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -40 }
-      ]
-    },{
-      featureType: 'transit.station',
-      stylers: [
-        { weight: 9 },
-        { hue: '#e85113' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'labels.icon',
-      stylers: [
-        { visibility: 'off' }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.stroke',
-      stylers: [
-        { lightness: 100 }
-      ]
-    },{
-      featureType: 'water',
-      elementType: 'labels.text.fill',
-      stylers: [
-        { lightness: -100 }
-      ]
-    },{
-      featureType: 'poi',
-      elementType: 'geometry',
-      stylers: [
-        { visibility: 'on' },
-        { color: '#f0e4d3' }
-      ]
-    },{
-      featureType: 'road.highway',
-      elementType: 'geometry.fill',
-      stylers: [
-        { color: '#efe9e4' },
-        { lightness: -25 }
-      ]
-    }
-  ];
+  // Map style credit: https://snazzymaps.com/style/42/apple-maps-esque
+  var styles = [{
+    "featureType": "landscape.man_made",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#f7f1df"
+    }]
+  },
+  {
+    "featureType": "landscape.natural",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#d0e3b4"
+    }]
+  },
+  {
+    "featureType": "landscape.natural.terrain",
+    "elementType": "geometry",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "poi",
+    "elementType": "labels",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "poi.business",
+    "elementType": "all",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "poi.medical",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#fbd3da"
+    }]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#bde6ab"
+    }]
+  },
+  {
+    "featureType": "road",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "road",
+    "elementType": "labels",
+    "stylers": [{
+      "visibility": "off"
+    }]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#ffe15f"
+    }]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry.stroke",
+    "stylers": [{
+      "color": "#efd151"
+    }]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#ffffff"
+    }]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "black"
+    }]
+  },
+  {
+    "featureType": "transit.station.airport",
+    "elementType": "geometry.fill",
+    "stylers": [{
+      "color": "#cfb2db"
+    }]
+  },
+  {
+    "featureType": "water",
+    "elementType": "geometry",
+    "stylers": [{
+      "color": "#a2daf2"
+    }]
+  }
+];
 
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
