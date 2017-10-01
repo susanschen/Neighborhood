37a033572b1ef4ade98a1ba9a9568a864a65d81d
Susan Chen <30938981+susanschen@users.noreply.github.com>
2017-10-01 18:17:34 -0400

    Move attraction data to external js

---
+++ data.js
@@ -0,0 +1,12 @@
+/**
+ * The array for the Attraction Model to hold location details
+ */
+
+var attractionsData = [
+  {"title": "Central Park", "location": {"lat": 40.767852, "lng": -73.979694}, "category": "Parks", "marker": null, "wikiUrl": "", "wikiText": ""},
+  {"title": "Metropolitan Museum of Art", "location": {"lat": 40.779437, "lng": -73.963244}, "category": "Buildings", "marker": null, "wikiUrl": "", "wikiText": ""},
+  {"title": "Prospect Park Zoo", "location": {"lat": 40.665375, "lng": -73.965414}, "category": "Parks", "marker": null, "wikiUrl": "", "wikiText": ""},
+  {"title": "Times Square", "location": {"lat": 40.758895, "lng": -73.985131}, "category": "Buildings", "marker": null, "wikiUrl": "", "wikiText": ""},
+  {"title": "United Nations", "location": {"lat": 40.748876, "lng": -73.968009}, "category": "Buildings", "marker": null, "wikiUrl": "", "wikiText": ""},
+  {"title": "Empire State Building", "location": {"lat": 40.748541, "lng": -73.985758}, "category": "Buildings", "marker": null, "wikiUrl": "", "wikiText": ""}
+];
